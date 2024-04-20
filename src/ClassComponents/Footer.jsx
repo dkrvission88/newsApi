c70583c88f494f63eac529a703599229b789d532
DKRVISSION <dkrvission88@gmail.com>
2024-04-20 15:24:25 +0530

    newsApi

---
+++ src/ClassComponents/Footer.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return(
+            <>
+            <footer className='background text-light text-center p-2 mt-2'>
+                <p>copyright@newsappllication by.com</p>
+                <p>By:dkrvission88@gmail.com</p>
+                
+            </footer>
+            
+            </>
+        )
+    }
+}
